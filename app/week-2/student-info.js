dc913bd46eb3922c3c86dcc677629c95795df950
Mona <xiaomei.mona.he@gmail.com>
2025-09-09 14:15:19 -0600

    feat: add student info component

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return(
+        <main>
+            <h1>CPRG306 Assignments</h1>
+            <ul>
+                <li><Link className="text-pink-600 hover:text-emerald-500" href="https://github.com/Mona121212?tab=repositories"> Mona github</Link></li>
+            </ul>
+        </main>
+    )
+}
