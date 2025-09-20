0d20d29bad9963a86379545b0f661c17433746ab
Mona <xiaomei.mona.he@gmail.com>
2025-09-19 23:55:05 -0600

    feat: week 3 assignment- object and props

--- app/page.js
+++ app/page.js
@@ -6,7 +6,10 @@ export default function Home() {
         <main className="flex flex-col items-center justify-center min-h-screen">
             <ul>
                 <li>
-                    <Link className="text-shadow-blue-500 hover:text-amber-400 text-4xl" href="./week-2">CPRG 306: Web Development2 - Assignments</Link>
+                    <Link className="text-shadow-blue-500 hover:text-amber-400 text-4xl" href="./week-2">Week Two</Link>
+                    <br></br>
+                    <Link className="text-shadow-blue-500 hover:text-amber-400 text-4xl" href="./week-3">Week Three</Link>
+
                 </li>
             </ul>
         </main>
---
+++ app/week-3/item.js
@@ -0,0 +1,19 @@
+export default function Item(props) {
+   
+   let {name, quantity, category} = props;
+    return (
+        <li className="flex items-center p-4 bg-slate-200 max-w-sm m-2">
+           <div className="flex-1">
+                <div className="text-lg font-semibold ml-4">{name}</div>
+                <div className="text-lg font-semibold ml-4 ">Category: {category}</div> 
+                <div className="text-lg font-semibold ml-4">   Quantity: {quantity}</div>
+            </div>
+            
+
+            
+        
+        </li>
+        
+
+    )
+}
