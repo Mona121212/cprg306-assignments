88897544bfcdd7baaee68b14289655575b38babe
Mona <xiaomei.mona.he@gmail.com>
2025-09-26 13:08:35 -0600

    feat: add tailwindcss

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -17,19 +17,22 @@ export default function NewItem() {
   };
 
   let buttonStyle =
-    "bg-pink-200 text-white hover:bg-fuchsia-600 active:bg-amber-600 p-4 m-4 cursor-pointer";
+    "w-8 p-1 bg-amber-600 text-white font-semibold rounded-full shadow-md hover:bg-amber-500 focus:outline-none";
 
   return (
-    <main>
-      <p> {quantity}</p>
-      <button className={buttonStyle} onClick={decrement}>
-        {" "}
-        -{" "}
-      </button>
-      <button className={buttonStyle} onClick={increment}>
-        {" "}
-        +{" "}
-      </button>
+    <main className="flex justify-center items-center m-100 w-full">
+      <div className="p-4 m-20 bg-gray-700 rounded-lg text-white w-40">
+        <div className="flex justify-between">
+           <p className="text-blue-50 m-1"> {quantity}</p>
+          <button className={buttonStyle} onClick={decrement}>
+          -
+        </button>
+        <button className={buttonStyle} onClick={increment}>
+          +
+        </button>
+       </div>
+      
+      </div>
     </main>
   );
 }
