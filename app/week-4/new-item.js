b2588bd7599b54e196aa9cc748b8fa1c0e905ed4
Mona <xiaomei.mona.he@gmail.com>
2025-10-12 16:56:35 -0600

    fix: fix files code

---
+++ app/week-4/new-item.js
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+export default function NewItem() {
+  const [quantity, setQuantity] = useState(1);
+
+  const increment = () => {
+    if (quantity < 20) {
+      setQuantity(quantity + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  let buttonStyle =
+    "w-8 p-1 bg-amber-600 text-white font-semibold rounded-full shadow-md hover:bg-amber-500 focus:outline-none";
+
+  return (
+    <main className="flex justify-center items-center m-100 w-full">
+      <div className="p-4 m-20 bg-gray-700 rounded-lg text-white w-40">
+        <div className="flex justify-between">
+           <p className="text-blue-50 m-1"> {quantity}</p>
+          <button className={buttonStyle} onClick={decrement}>
+          -
+        </button>
+        <button className={buttonStyle} onClick={increment}>
+          +
+        </button>
+       </div>
+      
+      </div>
+    </main>
+  );
+}
+
+
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,38 +1,9 @@
-"use client";
+import NewItem from "./new-item";
 
-import { useState } from "react";
-export default function NewItem() {
-  const [quantity, setQuantity] = useState(1);
-
-  const increment = () => {
-    if (quantity < 20) {
-      setQuantity(quantity + 1);
-    }
-  };
-
-  const decrement = () => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1);
-    }
-  };
-
-  let buttonStyle =
-    "w-8 p-1 bg-amber-600 text-white font-semibold rounded-full shadow-md hover:bg-amber-500 focus:outline-none";
-
+export default function Page() {
   return (
-    <main className="flex justify-center items-center m-100 w-full">
-      <div className="p-4 m-20 bg-gray-700 rounded-lg text-white w-40">
-        <div className="flex justify-between">
-           <p className="text-blue-50 m-1"> {quantity}</p>
-          <button className={buttonStyle} onClick={decrement}>
-          -
-        </button>
-        <button className={buttonStyle} onClick={increment}>
-          +
-        </button>
-       </div>
-      
-      </div>
+    <main>
+      <NewItem />
     </main>
   );
 }
